7c3a4ffeffd4707fe8d76e32795cbabe9c48b973
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-18 10:27:21 -0300

    Adição de metodos equals, hashCode e construtor2
    Adicionei os metodos equals, hascode e um construtor que serve para busca para realizar operações de remoção, por exemplo.
    
    Os metodos equals e hashcode eram necessarios para realizar algumas comparações, como por exemplo, no metodo contains.

--- ComponenteCurricular.java
+++ ComponenteCurricular.java
@@ -4,16 +4,62 @@ public class ComponenteCurricular {
     private String nome;
     private int ID;
 
-    public ComponenteCurricular(int cargaHoraria, String nome, int iD) {
-        this.cargaHoraria = cargaHoraria;
+    public ComponenteCurricular(int cargaHoraria, String nome, int ID) {
+            this.nome = nome;
+            this.ID = ID;
+            if(cargaHoraria < 40){
+                this.cargaHoraria = 30;
+            }  else if (cargaHoraria >= 40){
+                this.cargaHoraria = 60;
+            } else {
+                this.cargaHoraria = cargaHoraria;
+            }
+    }
+    //Construtor usado somente para pesquisar e comparar o objeto instanciado a qual se quer remover ou adicionar
+    public ComponenteCurricular(String nome, int ID){
         this.nome = nome;
-        ID = iD;
+        this.ID = ID;
     }
 
+    //Metodo que será posteriormente utilizado em algumas operações
     public int getCargaHoraria() {
         return this.cargaHoraria;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + cargaHoraria;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ID;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ComponenteCurricular other = (ComponenteCurricular) obj;
+        
+        //Realizei uma pequena alteração no metodo equals para que não precisemos comparar a carga horaria de cada componente
+        /*if (cargaHoraria != other.cargaHoraria)
+            return false;*/
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (ID != other.ID)
+            return false;
+        return true;
+    }
+
+    
     @Override
     public String toString() {
         return "Carga horaria: " + cargaHoraria + " nome do componente: " + nome + " ID: " + ID;
