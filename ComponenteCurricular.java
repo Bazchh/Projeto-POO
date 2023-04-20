7ffe63890d7e228ca1477722ce84daee05b3945e
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-20 10:57:30 -0300

    Adição e modificação em metodos e atributos
    Modifiquei alguns atributos, adicionei alguns, removi outros. Modifiquei alguns metodos, adicionei o metodo para se adicionar uma turma para o componente curricular. Modificação do construtor. Implementação de um gerador aleatorio de ID.

--- ComponenteCurricular.java
+++ ComponenteCurricular.java
@@ -1,38 +1,52 @@
+import java.util.LinkedList;
+import java.util.Random;
 
 public class ComponenteCurricular {
+    private int nDeTurmasDoComponente = 0;
     private int cargaHoraria;
     private String nome;
-    private int ID;
+    // Id para criar um componente curricular
+    private final String ID = geradorAleatorioIdComponente();
+    // Id para buscar, comparar ou remover um componente curricular
+    private String idBusca;
+    private LinkedList<Turma> turmaDaDisciplina = new LinkedList<>();
 
-    public ComponenteCurricular(int cargaHoraria, String nome, int ID) {
-            this.nome = nome;
-            this.ID = ID;
-            if(cargaHoraria < 40){
-                this.cargaHoraria = 30;
-            }  else if (cargaHoraria >= 40){
-                this.cargaHoraria = 60;
-            } else {
-                this.cargaHoraria = cargaHoraria;
-            }
-    }
-    //Construtor usado somente para pesquisar e comparar o objeto instanciado a qual se quer remover ou adicionar
-    public ComponenteCurricular(String nome, int ID){
+    public ComponenteCurricular(int cargaHoraria, String nome) {
         this.nome = nome;
-        this.ID = ID;
+        // Condições para definição da carga horaria de um componente curricular
+        if (cargaHoraria < 40) {
+            this.cargaHoraria = 30;
+        } else if (cargaHoraria >= 40) {
+            this.cargaHoraria = 60;
+        } else {
+            this.cargaHoraria = cargaHoraria;
+        }
     }
 
-    //Metodo que será posteriormente utilizado em algumas operações
+    // Construtor usado somente para pesquisar e comparar o objeto instanciado a
+    // qual se quer remover ou adicionar
+    public ComponenteCurricular(String nome, String idBusca) {
+        this.nome = nome;
+        this.idBusca = idBusca;
+    }
+
+    // Metodo que será posteriormente utilizado em algumas operações
     public int getCargaHoraria() {
         return this.cargaHoraria;
+    }
+
+    public void addTurmaParaOComponente() {
+        this.nDeTurmasDoComponente++;
+        Turma novaTurma = new Turma(this.nome + " T nº" + this.nDeTurmasDoComponente);
+        turmaDaDisciplina.add(novaTurma);
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + cargaHoraria;
         result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        result = prime * result + ID;
+        result = prime * result + ((ID == null) ? 0 : ID.hashCode());
         return result;
     }
 
@@ -45,24 +59,29 @@ public class ComponenteCurricular {
         if (getClass() != obj.getClass())
             return false;
         ComponenteCurricular other = (ComponenteCurricular) obj;
-        
-        //Realizei uma pequena alteração no metodo equals para que não precisemos comparar a carga horaria de cada componente
-        /*if (cargaHoraria != other.cargaHoraria)
-            return false;*/
         if (nome == null) {
             if (other.nome != null)
                 return false;
         } else if (!nome.equals(other.nome))
             return false;
-        if (ID != other.ID)
+        if (ID == null) {
+            if (other.ID != null)
+                return false;
+        } else if (!ID.equals(other.ID))
             return false;
         return true;
     }
 
-    
     @Override
     public String toString() {
         return "Carga horaria: " + cargaHoraria + " nome do componente: " + nome + " ID: " + ID;
+    }
+
+    final private static String geradorAleatorioIdComponente() {
+        Random random = new Random();
+        int n = random.nextInt(600000);
+        String str = Integer.toString(n);
+        return str;
     }
 
 }
