63345fa5d1a97ee07aed01bded73726dac237da4
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-24 17:09:49 -0300

    Add de lista de titulo dos professores

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        Professor p = new Professor("Marcos");
-        
-       p.adicionaComponenteCurricular(60, "Teste de software", "34234");
-       System.out.println(p.toString());
-       p.adicionarTurmaAoComponenteDoProfessor("Teste de software", "34234");
+        Connection connection = null;
+        try {
+          // Estabelecendo conexão com o banco de dados
+          connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/seu_banco_de_dados", "seu_usuario", "sua_senha");
+          System.out.println("Conexão bem-sucedida!");
+        } catch (SQLException e) {
+          System.out.println("Falha ao conectar com o banco de dados.");
+          e.printStackTrace();
+        } finally {
+          try {
+            if (connection != null) {
+              // Fechando a conexão
+              connection.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        }
+      }
     }
-}
