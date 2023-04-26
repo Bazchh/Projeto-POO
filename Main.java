0d8b76f4f897cd2a831aee8c59ef075769965160
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-26 16:26:59 -0300

    add de bibliotecas
    bibliotecas para realizar consultas em sql e inserir dados no banco de dados

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +11,7 @@ public class Main {
         Connection connection = null;
         try {
           // Estabelecendo conexão com o banco de dados
-          connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "tecmint", "12345678");
+          connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "mikael", "123456789");
           System.out.println("Conexão bem-sucedida!");
         } catch (SQLException e) {
           System.out.println("Falha ao conectar com o banco de dados.");
